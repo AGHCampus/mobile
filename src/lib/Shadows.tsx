c8de3081fb1377b5ef62e717f4e4df53b2732e2f
Gabriel Kazmierczak <gabriel.kazmierczak@ext.chime.com>
2023-06-25 17:40:06 +0200

    undo shadow changes

--- src/lib/Shadows.tsx
+++ src/lib/Shadows.tsx
@@ -14,7 +14,7 @@ export const Shadows = {
         shadowColor: '#000',
         shadowOffset: {
             width: 0,
-            height: 0,
+            height: 1,
         },
         shadowOpacity: 0.2,
         shadowRadius: 1.41,
